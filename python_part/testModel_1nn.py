7f66ef72995258b0cca29a334a14ee0d8c4fc41c
Chih-Wei Wu <chih-wei@lawn-143-215-82-144.lawn.gatech.edu>
2017-04-04 21:58:02 -0400

    change testModel functions

---
+++ python_part/testModel_1nn.py
@@ -0,0 +1,50 @@
+'''
+DNN playground using KERAS
+test DNN model on ENST dataset
+CW @ GTCMT 2017
+'''
+import numpy as np
+from FileUtil import getFilePathList
+from scipy.io import loadmat
+from keras.models import load_model
+
+'''
+==== User input
+'''
+targetDrummers = ['drummer1',
+                'drummer2',
+                'drummer3']
+parentFolder     = '../../unlabeledDrumDataset/evaluation_enst/STFT/'
+saveParentFolder = '../../unlabeledDrumDataset/evaluation_enst/Activations/'
+# parentFolder     = '/Volumes/CW_MBP15/Datasets/unlabeledDrumDataset/evaluation_enst/STFT/'
+# saveParentFolder = '/Volumes/CW_MBP15/Datasets/unlabeledDrumDataset/evaluation_enst/Activations/'
+modelpath = './models/dnn_model_1nn.h5'
+
+'''
+==== File IO + testing
+'''
+
+model = load_model(modelpath)
+
+for drummer in targetDrummers:
+    #==== get STFT
+    stftpath = parentFolder + drummer + '/'
+    stftFilePathList = getFilePathList(stftpath, 'mat')
+    saveFolder = saveParentFolder + drummer + '/'
+
+
+    for i in range(0, len(stftFilePathList)):
+        print 'test on file %f' % i
+        filename = stftFilePathList[i][-11:-4]
+        savepath = saveFolder + filename
+        tmp = loadmat(stftFilePathList[i])
+        X = np.ndarray.transpose(tmp['X'])
+
+        '''
+        ==== Testing
+        '''
+        Y = model.predict(X, batch_size = 32)
+        all = [Y[:, 0], Y[:, 1], Y[:, 2]]
+        np.save(savepath, all)
+
+
